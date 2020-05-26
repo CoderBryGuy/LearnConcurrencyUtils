5ab13ab1ad6327ec2ededfecff50fc0745e8c83c
Bryan <bryan.dov.bergman@gmail.com>
2020-05-26 21:19:25 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BlockingQueue<Integer> questionQueue = new ArrayBlockingQueue<Integer>(5);
+        Thread t1 = new Thread(new Producer(questionQueue));
+        Thread t2 = new Thread(new Consumer(questionQueue));
+
+        t1.start();
+        t2.start();
+    }
+}
